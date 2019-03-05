13f9e3cdf85af82177d2d24285043dd9cf8702fc
BarLesh <barlesh8@gmail.com>
2019-03-06 00:34:57 +0200

    working :)

--- client/app/client.js
+++ client/app/client.js
@@ -1,18 +1,27 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
-let  ws
+const ws_server = process.env.SERVER_URL
+const ws_protocol_prefix = "ws://"
+let ws;
 
-try{
-   ws = new WebSocket('ws://localhost:8080');
-} catch(e) {
-  console.error(e)
-  process.exit(1)
+console.log("client calling for server %s", ws_server);
+
+try {
+  ws = new WebSocket(ws_protocol_prefix + ws_server);
+} catch (e) {
+  console.error("error:", e);
+  process.exit(1);
 }
- 
-ws.on('open', function open() {
-  ws.send('Maccabi');
-});
- 
-ws.on('message', function incoming(data) {
-  console.log("client: %s", data);
-});
+
+try {
+  ws.on("open", function open() {
+    ws.send("Maccabi");
+  });
+
+  ws.on("message", function incoming(data) {
+    console.log("client: %s", data);
+  });
+} catch (e) {
+  console.error("error:", e);
+  process.exit(1);
+}
--- server/app/server.js
+++ server/app/server.js
@@ -1,5 +1,7 @@
 const WebSocket = require('ws');
  
+console.log("server.....")
+
 const wss = new WebSocket.Server({ port: 8080 });
  
 wss.on('connection', function connection(ws) {
